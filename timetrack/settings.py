4f223373dbcaca689adecbbc5d8a1ea984577252
Andres Vertel <avertel@gmail.com>
2024-11-10 15:59:48 -0500

    se confirgura la ruta static en settings

--- timetrack/settings.py
+++ timetrack/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'timetrack.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ["templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
