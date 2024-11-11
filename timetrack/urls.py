f9580391b5e57b3c01bdd0518f1325bedba3dc4a
Andres Vertel <avertel@gmail.com>
2024-11-11 14:35:50 -0500

    chore: se actualiza el archivo urls.py

--- timetrack/urls.py
+++ timetrack/urls.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
-from timetrack import views
+from . import views
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", views.index, name="index"),
+    path('', views.index, name="index"),
     # path("registro_horas/", include("recordHours.urls")),
 ]
