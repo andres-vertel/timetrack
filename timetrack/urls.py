851227e3f8c1a4115225ebc61d5a550af8f84efb
Andres Vertel <avertel@gmail.com>
2024-11-11 16:32:05 -0500

    chore: se añaden al proyecto las aplicaciones: recordHours, requestExpiredRegistration y projects, en las cuales se inclura la funcionalidad para dichas pestañas

--- timetrack/urls.py
+++ timetrack/urls.py
@@ -6,5 +6,7 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name="index"),
-    # path("registro_horas/", include("recordHours.urls")),
+    path("recordHours/", include("recordHours.urls")),
+    path("requestExpiredRegistration/", include("requestExpiredRegistration.urls")),
+    path('projects/', include('projects.urls')),
 ]
